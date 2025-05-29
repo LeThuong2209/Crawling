c0526087b609cc11c05d3d7129a2cbc8a4e2273d
Lê Thường <trandung@MacBook-Pro-cua-Tran.local>
2025-05-30 00:21:15 +0700

    update

--- crawl_pdf.py
+++ crawl_pdf.py
@@ -1,8 +1,27 @@
 import os
 import requests
+import pypdf
+import re
+
+def pdf_fil(pdf : str):
+    with open(pdf, 'rb') as pdf:
+        reader = pypdf.PdfReader(pdf, strict=False)
+        pdf_text = []
+        
+        for page in reader.pages:
+            content = page.extract_text()
+            pdf_text.append(content)
+        return pdf_text
 
 url = "https://proceedings.mlr.press/v26/nicol12a/nicol12a.pdf"
 response = requests.get(url)
 if response.status_code == 200:
     with open("this.pdf", 'wb') as f:
         f.write(response.content)   
+
+if __name__ == '__main__':
+    extracted_text = pdf_fil("this.pdf")
+    for text in extracted_text:
+        split_message = re.split(r'\s+|[,i?!.-]\s*', text.lower())
+        if 'gmail' in split_message:
+            print(split_message)
